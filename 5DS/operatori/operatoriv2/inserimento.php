ce398eec6c7bb3ed288c274985d2a64b2efdaa15
loris.martini <martini.loloris@gmail.com>
2025-02-17 12:29:41 +0100

    sviluppo

--- 5DS/operatori/operatoriv2/inserimento.php
+++ 5DS/operatori/operatoriv2/inserimento.php
@@ -13,7 +13,6 @@
     <!-- CSS di Bootstrap -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<!-- JavaScript di Bootstrap (necessario per componenti interattivi) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <style>
